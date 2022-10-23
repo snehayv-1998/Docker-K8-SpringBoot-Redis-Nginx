2626ceb32f37f6f65a24c036146f7002a301e961
sneha <sneha.yv@robosoftin.com>
2022-10-23 21:51:20 +0530

    Containerizing spring boot application

---
+++ springbootWithDocker/src/test/java/com/example/springbootWithDocker/SpringbootWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootWithDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
